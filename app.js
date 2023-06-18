5ff1199def0e0222fd3775f20447070344516f9c
Hanerlan <hk.erlandsen@gmail.com>
2023-06-18 13:23:07 +0200

    external libraries added and installed

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var memeRouter = require('./routes/meme');
+var memesRouter = require('./routes/memes');
 
 var app = express();
 
@@ -18,9 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/meme', memeRouter);
+app.use('/memes', memesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
