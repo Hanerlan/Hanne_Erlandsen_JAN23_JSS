55aaa4390d49f57dd130f48451adb9467e3d16fe
Hanerlan <hk.erlandsen@gmail.com>
2023-06-23 10:20:48 +0200

    Login userface

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 var memesRouter = require('./routes/memes');
 
 
@@ -25,6 +26,7 @@ app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 app.use('/memesOverview', memesRouter);
 app.use('/memeDetails', memesRouter);
 
